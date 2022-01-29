46eabdc71f86285f563f86f9726689890387ae55
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-01-29 16:42:46 -0600

    :(

--- CSSA Game Jam 2022/draggables/drag_it.gd
+++ CSSA Game Jam 2022/draggables/drag_it.gd
@@ -1,10 +1,5 @@
 extends Spatial
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print('huh does this work')
-
-
 func _on_Draggable_drag_move(_node, cast):
 	set_translation(cast['position'])
