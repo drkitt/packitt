248bf739438e0be6e81f82ddf465d47abd9cdf39
Alex Kitt <alex.kitt@outlook.com>
2022-01-29 21:54:53 -0600

    Add error handling

--- CSSA Game Jam 2022/title_screen.gd
+++ CSSA Game Jam 2022/title_screen.gd
@@ -2,7 +2,12 @@ extends MarginContainer
 
 
 func _on_Play_button_pressed() -> void:
-	get_tree().change_scene("res://SortingScreen.tscn")
+	var result = get_tree().change_scene("res://SortingScreen.tscn")
+	match result:
+		ERR_CANT_OPEN:
+			print_debug("Could not open sorting screen")
+		ERR_CANT_CREATE:
+			print_debug("Could not instantiate sorting screen")
 
 
 func _on_Quit_button_pressed() -> void:
