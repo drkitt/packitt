11a3d96483d7a4133c774d8eb0571f24de0d91bf
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-01-30 15:49:44 -0600

    music for the sorting screen

--- CSSA Game Jam 2022/sorting_screen.gd
+++ CSSA Game Jam 2022/sorting_screen.gd
@@ -5,8 +5,15 @@ func _on_StartButton_pressed() -> void:
 	$Wiggler.wiggling = true
 	$Timer.start()
 	$UI/StartButton.hide()
+	$AudioStreamPlayer.stream = preload("res://music/francis-battle.mp3")
+	$AudioStreamPlayer.volume_db = -15
+	$AudioStreamPlayer.play()
 
 
 func _on_Timer_timeout():
 	$Wiggler.wiggling = false
 	$UI/EndScreen.show()
+	$AudioStreamPlayer.stream = preload("res://music/joshua-mclean_celebrate.ogg")
+	$AudioStreamPlayer.volume_db = -15
+	$AudioStreamPlayer.play()
+	$Timer.stop()
