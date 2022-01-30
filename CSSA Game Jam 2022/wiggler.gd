a8c01b3aa6323debd34e39ddfd5f423b987d6cf3
Alex Kitt <alex.kitt@outlook.com>
2022-01-29 21:57:12 -0600

    Backport phase to spatial wiggler

--- CSSA Game Jam 2022/wiggler.gd
+++ CSSA Game Jam 2022/wiggler.gd
@@ -4,10 +4,11 @@ extends Spatial
 export(bool) var wiggling = true
 export(float) var frequency = 1
 export(float) var amplitude = 1
+export(float) var phase = 0
 
 var accumulator: float = 0
 
 func _process(delta: float) -> void:
 	if wiggling:
 		accumulator += delta
-		$Box.translation.y = amplitude * sin(2 * PI * frequency * accumulator)
+		$Box.translation.y = amplitude * sin(2 * PI * frequency * accumulator + phase)
