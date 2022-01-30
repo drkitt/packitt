eefd866903d8135420df03522d7cdb360d77f396
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2022-01-30 17:53:35 -0600

    yeah it score

--- CSSA Game Jam 2022/sorting_screen.gd
+++ CSSA Game Jam 2022/sorting_screen.gd
@@ -1,5 +1,7 @@
 extends WorldEnvironment
 
+export var score = 5
+var fell = []
 
 func _on_StartButton_pressed() -> void:
 	$Wiggler.wiggling = true
@@ -11,6 +13,8 @@ func _on_StartButton_pressed() -> void:
 #	$Trapdoor/CollisionShape.disabled = true
 #	$Trapdoor.queue_free()
 	$Trapdoor.translation.y = -1
+	$Wood_Table/Area.monitoring = true
+	$Draggables.translate(Vector3(0,0.01,0))
 
 
 func _on_Timer_timeout():
@@ -20,3 +24,10 @@ func _on_Timer_timeout():
 	$AudioStreamPlayer.volume_db = -15
 	$AudioStreamPlayer.play()
 	$Timer.stop()
+
+
+func _on_Area_body_entered(body):
+	if not body in fell and body.get_parent() in $Draggables.get_children():
+		score -= 1
+		print(score)
+		fell.append(body)
